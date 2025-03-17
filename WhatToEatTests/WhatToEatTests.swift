a20dc58c90844740178bc2b5de53451c5fed4fac
Darrin-Lin <darrin.de88@gmail.com>
2025-03-18 03:13:28 +0800

    Initial Commit

---
+++ WhatToEatTests/WhatToEatTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhatToEatTests.swift
+//  WhatToEatTests
+//
+//  Created by Darrin Lin on 2025/3/18.
+//
+
+import Testing
+@testable import WhatToEat
+
+struct WhatToEatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
